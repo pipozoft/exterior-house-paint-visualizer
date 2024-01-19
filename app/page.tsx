213f1f80d7720127e833bbccfbf4790dfba60a4f
Jose L Pimienta <pipozoft@gmail.com>
2024-01-18 23:00:20 -0500

    Add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Logo from "@/components/brand/logo";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="min-h-screen grid grid-cols-5">
       <div className="col-span-2 p-8">
-        <h1 className="text-xl">Exterior House Paint Visualizer</h1>
+        <Logo />
       </div>
       <div className="col-span-3 h-screen bg-gray-200"></div>
     </main>
